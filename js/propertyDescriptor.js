4d901575cfc023458c1adf7e070207f24e68750e
ZachyDev <mosetizachary001@gmail.com>
2020-06-12 18:39:03 +0300

    descriptor property in js

--- js/propertyDescriptor.js
+++ js/propertyDescriptor.js
@@ -1,9 +1,13 @@
 'use strict';
 const car = {
-    "brand": "Toyota",
+    "brand": { "v1": "toyota2","v2":'premio'},
     "model": "Range",
     output(){
         return Object.getOwnPropertyDescriptor(car,"brand");
     }
 }
+Object.defineProperty(car,"brand",{ writable: false })
+Object.freeze(car.brand)
+car.brand.v1 = "isuzu" ;
+console.log(car.brand)
 console.log(car.output())
